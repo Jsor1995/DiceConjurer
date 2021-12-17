11c084c4db0673a2dede569901d31aa2dad6abb4
Jsor1995 <[justinsor@gmail.com]>
2021-12-16 16:34:50 -0800

    12/16

--- CONJURE/static/statblock.js
+++ CONJURE/static/statblock.js
@@ -7,8 +7,11 @@ $(document).ready(function () {
             var response = await fetch(`/create_card/${animal}`);
             var result = await response.json();
             console.log(result);
-            statblock(result);
+            cardCount(result);
+            console.log(result.actions);
+            console.log(diceRoll(20));
             $(".card").toggle();
+            
         }
         catch {
             console.log("Error: Could Not Retreive Statblock")
@@ -17,11 +20,11 @@ $(document).ready(function () {
 });
 
 function statblock(data) {
+    console.log("test");
     var title = data.name;
     var ac = data.armor_class;
     var cr = data.challenge_rating;
-
-    $(".card-deck").html(
+    var cardhtml = 
         `<div class="card ">
             <div class="card-body">
                 <h5 class="card-title">${title}</h5>
@@ -32,8 +35,47 @@ function statblock(data) {
             </ul>
             <div class="card-body dice-button">
                 <a href="#" class="btn btn-primary">Attack</a>
-                <a href="#" class="btn btn-primary">Damage</a>
             </div>
-        </div>`);
-    document.body.appendChild(card);
+        </div>`;
+    
+    $(".card-deck").append(cardhtml);
+    return;
+}
+
+function cardCount(data) {
+    var cr = data.challenge_rating;
+    var numIter;
+    if (cr <= 0.25) {
+        numIter = 8;
+    }
+    else if (cr <= 0.5) {
+        numIter = 4;
+    }
+    else if (cr <= 1) {
+        numIter = 2;
+
+    }
+    else if (cr <= 2) {
+        numIter = 1;
+    }
+    else {
+        console.log("Challenge Rating Not Found");
+
+    }
+    $(".card-deck").empty();
+
+    for (var i = 0; i < numIter; i++) {
+        console.log(i);
+        statblock(data);
+    }
+    $(".card").toggle();
+};
+
+function diceRoll(formula) {
+    console.log("Rolling");
+    var roll = Math.floor( Math.random() * formula) + 1; 
+    var result = droll.roll('3d6+1');
+
+    console.log(result);
+    return roll;
 }
